adf9d0d851a4f9254245635710a4569fa1ed6531
somyarsahu <somya.sahu@quadbtech.com>
2024-03-28 12:02:01 +0530

    Added customButton class

--- lib/flutter_btn.dart
+++ lib/flutter_btn.dart
@@ -1,7 +1,28 @@
 library flutter_btn;
 
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
+import 'package:flutter/material.dart';
+
+class CustomButton extends StatelessWidget {
+  const CustomButton({required Key key, @required this.onPressed, required this.child, this.style})
+      : super(key: key);
+
+  final onPressed;
+  final Widget child;
+  final style;
+
+  @override
+  Widget build(BuildContext context) {
+    return TextButton(
+        onPressed: onPressed,
+        style: TextButton.styleFrom(
+          foregroundColor: Colors.white, padding: const EdgeInsets.all(16.0),
+          backgroundColor: Colors.blue,
+          elevation: 9.0,
+          textStyle: const TextStyle(
+            fontSize: 20,
+          ),
+        ),
+        child: child
+    );
+  }
 }
